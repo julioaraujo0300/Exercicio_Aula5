9347a70d27460183faff73a5d425000943163186
julioaraujo0300 <80927029+julioaraujo0300@users.noreply.github.com>
2021-04-16 17:13:24 +0100

    job done

--- ex_aula05.js
+++ ex_aula05.js
@@ -5,11 +5,25 @@ let writeOutput = function(text){
 
 let truncateString = function(str, num)
 {
-    if(str.length > num){
-        str = str.slice(0, num);
+    if(str.length <= num){
         writeOutput(str);
     }
-    writeOutput(str.length);
+
+    if(str.length > num){
+        str = str.slice(0, num);
+        writeOutput(str + "...");
+    }
 }
 
-truncateString("sup doods, o meu nome é wuant", 10);
+truncateString("Adoro programação e animação web", 5);
+
+let titleCase = function(str){
+    let separatedString = str.split(' ');
+    for (let i = 0; i < separatedString.length; i++){
+        separatedString[i] = separatedString[i].charAt(0).toUpperCase() + separatedString[i].substring(1);
+    } 
+    let finishedString = separatedString.join(' ');
+    writeOutput(finishedString);
+}
+
+titleCase("o meu nome é júlio simaõ araújo");
